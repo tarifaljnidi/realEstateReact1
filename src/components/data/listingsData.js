c964af4dd0dacc2c9d3687657d3c5c14cd5e4a6c
tarifaljnidi <tarifaljnidi1986@hotmail.com>
2021-12-06 10:31:27 +0100

    first commit

---
+++ src/components/data/listingsData.js
@@ -0,0 +1,98 @@
+var listingsData = [
+  {
+    address: 'Address 1',
+    city: 'City 1',
+    state: 'state 1',
+    rooms: 3,
+    price: 220000,
+    floorSpace: 2000,
+    extras: [
+      'gym'
+    ],
+    homeType: 'Apartment',
+    image: 'http://media.equityapartments.com/images/c_crop,x_0,y_0,w_1920,h_1080/c_fill,w_1920,h_1080/q_80/4206-28/the-kelvin-apartments-exterior.jpg'
+  },
+  {
+    address: 'Address 2',
+    city: 'City 2',
+    state: 'state 2',
+    rooms: 2,
+    price: 24666,
+    floorSpace: 1430,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Condo',
+    image: 'https://images.pexels.com/photos/439391/pexels-photo-439391.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+  },
+  {
+    address: 'Address 3',
+    city: 'City 3',
+    state: 'state 3',
+    rooms: 1,
+    price: 345355,
+    floorSpace: 2400,
+    extras: [
+      'elevator'
+
+    ],
+    homeType: 'Single Home',
+    image: 'https://c.o0bg.com/rf/image_960w/Boston/2011-2020/2017/01/09/BostonGlobe.com/Lifestyle/Images/doherty_10names04_liv.jpg'
+  },
+  {
+    address: 'Address 4',
+    city: 'City 4',
+    state: 'state 4',
+    rooms: 0,
+    price: 80000,
+    floorSpace: 1000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Studio',
+    image: 'http://media.equityapartments.com/images/q_50/f_auto/fl_lossy/685-28/oak-park-apartment-homes-kitchen'
+  },
+  {
+    address: 'Address 5',
+    city: 'City 5',
+    state: 'state 5',
+    rooms: 3,
+    price: 603000,
+    floorSpace: 3000,
+    extras: [
+      'gym'
+    ],
+    homeType: 'Condo',
+    image: 'https://q-xx.bstatic.com/xdata/images/hotel/840x460/135462587.jpg?k=da2d74b3ff7b3945459fd78de5953d8190706dc771a568da32509f986ee7fde8&o='
+  },
+  {
+    address: 'Address 6',
+    city: 'City 6',
+    state: 'state 6',
+    rooms: 2,
+    price: 220000,
+    floorSpace: 2000,
+    extras: [
+      'pool'
+    ],
+    homeType: 'Multi Home',
+    image: 'https://i.pinimg.com/736x/4a/a9/55/4aa955a400be6c95a34a61bb0094ba35.jpg'
+  },
+  {
+    address: 'Address 7',
+    city: 'City 7',
+    state: 'state 7',
+    rooms: 1,
+    price: 150000,
+    floorSpace: 2000,
+    extras: [
+      'gym'
+    ],
+    homeType: 'Room',
+    image: 'https://media.istockphoto.com/photos/modern-residential-buildings-picture-id1225226886?k=20&m=1225226886&s=612x612&w=0&h=7rF48nIZCssb48iArI2L2VaYOWhMWemFcg52NzngXwM='
+  }
+]
+
+export default listingsData;
